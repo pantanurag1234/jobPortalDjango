a52f5ed1b87185b331704f8f4c90f3f3576876de
pantanurag1234 <114347375+pantanurag1234@users.noreply.github.com>
2023-05-24 10:08:30 +0530

    added host 127.0.0.1 to settings.py

--- JobPortalDjango/settings.py
+++ JobPortalDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-nbm+w(67&t1icc&6uyrvhijt=uxgvh5f%i0vh^iwdnv5y5e7+c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
 
 # Application definition
